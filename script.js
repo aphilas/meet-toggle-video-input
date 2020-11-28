98ccec2ef40eedded0f13e504c306ca558dd8fd4
nevilleomangi <31406314+nevilleomangi@users.noreply.github.com>
2020-11-28 16:09:35 +0300

    await dialogs opening

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ const sendMessage = details => new Promise((resolve, reject) => {
   chrome.runtime.sendMessage(details, response => { 
     if (chrome.runtime.lastError) {
       reject(chrome.runtime.lastError)
-      // console.error()
       return false
     }
 
-    resolve(response) 
+    resolve(response)
   })
 })
 
@@ -17,8 +16,9 @@ const toggleVideo = async () => {
   
   if (!optionsEl) return false
   optionsEl.ariaExpanded == 'false' ? optionsEl.click() : null
+  optionsEl.style.pointerEvents = 'none' // disable on click
 
-  await sleep(200) // await dialog opening
+  await sleep(1500) // await dialog opening
 
   const settingsEl = document.querySelector(`[aria-label='Settings']`)
   const { top: offsetTop, right: offsetRight, bottom: offsetBottom, left: offsetLeft } = settingsEl.getBoundingClientRect()
@@ -28,33 +28,41 @@ const toggleVideo = async () => {
   }
 
   const clickRes = await sendMessage({ command: 'click-settings', coords })
-  if (!clickRes || !clickRes.data) return false
+  // if (!clickRes || !clickRes.data) return false
 
+  await sleep(500)
   const videoEl = document.querySelector(`[aria-label='Video']`)
   if (!videoEl) return false
   videoEl.click()
 
+  await sleep(500)
   const resolutionEl = document.querySelector(`[aria-label='Select receive bitrate']`)
   if (!resolutionEl) return false
   resolutionEl.children[0].click()
 
-  const selected = resolutionEl.children[1].children.findIndex(option => option.ariaSelected == 'true')
-  // 0 - auto, 1 - hd, 2 - sd, 3 - sd single, 4 - audio
-  selected == 4 ? resolutionEl.children[1].children[3].click() : resolutionEl.children[1].children[4].click()
+  await sleep(500)
+  const optionEls = Array.from(resolutionEl.children[1].children)
+
+  const selected = optionEls.findIndex(option => option.ariaSelected == 'true')
+  selected == optionEls.length - 1 ? optionEls[optionEls.length - 2].click() : optionEls[optionEls.length - 1].click()
+  // [0 - auto], 1 - hd, 2 - sd, 3 - sd single, 4 - audio
 
   const closeEl = document.querySelector(`[aria-label='Close']`)
   closeEl.click()
 
+  optionsEl.style.pointerEvents = 'auto' // disable on click
+  
   return true
 }
 
 const onMessage = async (request, sender) => {
-  if (request != 'toggle-video') return
+  if (request.command != 'toggle-video') return
   return await toggleVideo()
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  onMessage(request, sender).then(response => sendResponse(response))
+  // onMessage(request, sender)
+  onMessage(request, sender).then(ret => sendResponse({ command: 'toggle-video', data: ret }))
   return true
 })
 
